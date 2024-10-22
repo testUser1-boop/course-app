d507a5a6ce5ca58450d88044a5aab9c5bfd4606c
Rounak Kumar Arya <test.course.app1@gmail.com>
2024-10-22 22:35:50 +0530

    update root path

--- server/index.js
+++ server/index.js
@@ -40,10 +40,10 @@ app.use("/student/order", studentViewOrderRoutes);
 app.use("/student/courses-bought", studentCoursesRoutes);
 app.use("/student/course-progress", studentCourseProgressRoutes);
 
-app.use(express.static(path.join(__dirname, "/client/dist")));
+app.use(express.static(path.join(process.cwd(), "/client/dist")));
 
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "client", "dist", "index.html"));
+  res.sendFile(path.join(process.cwd(), "client", "dist", "index.html"));
 });
 
 app.use((err, req, res, next) => {
