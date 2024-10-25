5c8516a71bbcb08134dd049a3a067bffe4647ef8
Rounak Kumar Arya <test.course.app1@gmail.com>
2024-10-25 11:43:50 +0530

    base url change

--- client/src/api/axiosInstance.js
+++ client/src/api/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: "http://localhost:10000",
+  baseURL: "https://course-app-fv47.onrender.com/",
 });
 
 axiosInstance.interceptors.request.use(
